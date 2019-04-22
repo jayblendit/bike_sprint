589446af1ad3dc493d2d1b453b9c9b5fe1870dd3
jay <jay@blendit.eu>
2019-04-22 18:05:26 +0800

    fix error in services

--- config/services.php
+++ config/services.php
@@ -42,19 +42,19 @@ return [
             'secret' => env('STRIPE_WEBHOOK_SECRET'),
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
+    ],
 
     'google' => [
         'client_id'     => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect'      => env('GOOGLE_REDIRECT')
-        ],
+    ],
 
     'facebook' => [
        'client_id'     => env('FB_APP_ID'),
        'client_secret' => env('FB_APP_SECRET'),
        'redirect'      => env('FB_APP_REDIRECT'),
-      ],
+    ],
 
-  ],
 
 ];
