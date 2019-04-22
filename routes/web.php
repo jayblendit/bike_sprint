78f0a97c6ea6e27cf24643347ff3cb6ebc3fcf85
vccuasito <viacamillecuasito13@gmail.com>
2019-04-22 11:37:11 +0800

    login buttons

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/privacy-policy', 'PagesController@privacyPolicy')->name('privacy.policy');
