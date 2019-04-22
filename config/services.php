ba81c856a2edfd85168bb3af247b399b77c18267
CHRISTIAN <oerbachristian@gmail.com>
2019-04-22 17:03:57 +0800

    facebook login

--- config/services.php
+++ config/services.php
@@ -43,11 +43,18 @@ return [
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
         ],
 
+    'google' => [
+        'client_id'     => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect'      => env('GOOGLE_REDIRECT')
+        ],
+
     'facebook' => [
        'client_id' => env('FB_APP_ID'),
        'client_secret' => env('FB_APP_SECRET'),
        'redirect' => env('FB_APP_REDIRECT'),
       ],
-    ],
+
+  ],
 
 ];
