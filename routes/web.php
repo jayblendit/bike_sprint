51c92e5244199c2be5eed142667ccfebf6b02d5e
vccuasito <viacamillecuasito13@gmail.com>
2019-04-25 15:18:45 +0800

    create

--- routes/web.php
+++ routes/web.php
@@ -23,20 +23,27 @@ Route::get('/redirect', 'SocialAuthFacebookController@redirect');
 Route::get('/callback', 'SocialAuthFacebookController@callback');
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/ranking', 'PagesController@ranking')->name('ranking');
 Route::get('/rankingg', 'PagesController@rankingg')->name('rankingg');
 Route::get('/rankinggg', 'PagesController@rankinggg')->name('rankinggg');
+
 Route::get('/privacy-policy', 'PagesController@privacyPolicy')->name('privacy.policy');
+
 Route::get('/terms-and-conditions', 'PagesController@termsAndConditions')->name('terms.and.conditions');
+
 Route::get('/lobby', 'PagesController@lobby')->name('lobby');
+
 Route::get('/google/redirect', 'Auth\LoginController@googleRedirectToProvider')->name('google.redirect');
 Route::get('/google/callback', 'Auth\LoginController@googleHandleProviderCallback')->name('google.callback');
-Route::get('/bikes/serial', 'BikesController@index')->name('bike.serial');
-Route::get('/facebook/redirect', 'Auth\LoginController@facebookRedirectToProvider')->name('facebook.redirect');
 
+Route::get('/bikes/serial', 'BikesController@index')->name('bike.serial');
+
+Route::get('/facebook/redirect', 'Auth\LoginController@facebookRedirectToProvider')->name('facebook.redirect');
 Route::get('/facebook/callback', 'Auth\LoginController@facebookHandleProviderCallback')->name('facebook.callback');
+
 Route::get('/create', 'PagesController@create')->name('create');
 
-Route::get('/facebook/callback', 'Auth\LoginController@facebookHandleProviderCallback')->name('facebook.callbac');
+
 
 
