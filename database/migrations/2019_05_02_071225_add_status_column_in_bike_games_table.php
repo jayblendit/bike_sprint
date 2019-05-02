504c1295598e46597011fa7a15c1517b0f8e5ffa
CHRISTIAN <oerbachristian@gmail.com>
2019-05-02 15:40:22 +0800

    add status column in bike games table

--- database/migrations/2019_05_02_071225_add_status_column_in_bike_games_table.php
+++ database/migrations/2019_05_02_071225_add_status_column_in_bike_games_table.php
@@ -14,7 +14,7 @@ class AddStatusColumnInBikeGamesTable extends Migration
     public function up()
     {
         Schema::table('bike_games', function (Blueprint $table) {
-            //
+            $table->string('status');
         });
     }
 
