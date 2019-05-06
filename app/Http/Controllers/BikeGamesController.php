a22569530984cd9c1249797f7547e3ecf0f95bdb
vccuasito <viacamillecuasito13@gmail.com>
2019-05-06 11:48:44 +0800

    modify lobby function in pagesController

--- app/Http/Controllers/BikeGamesController.php
+++ app/Http/Controllers/BikeGamesController.php
@@ -5,26 +5,29 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Bikegame;
 
-class BikeGamesController extends Controller
-{
-    public function store(Request $request){
-    	
-    		$bikegame = new Bikegame;
-    		$bikegame->title = $request->title;
-			$bikegame->distance_id = 1;
-			$bikegame->created_by = 1;
-			$bikegame->status = 1;
-    		$bikegame->no_of_players = $request->no_of_players;
-    		$bikegame->save();
-    	
+class BikeGamesController extends Controller{
 
-    	return redirect()->route('bikegames.index');
+  public function store(Request $request){
+    	
+    $bikegame = new Bikegame;
+    $bikegame->title = $request->title;
+		$bikegame->distance_id = 1;
+		$bikegame->created_by = 1;
+		$bikegame->status = 1;
+    $bikegame->no_of_players = $request->no_of_players;
+    $bikegame->save();
+    	
+    return redirect()->route('bikegames.index');
 	}
 	
-	public function index()
-	{
+	public function index(){
 		$bike_games = BikeGame::all();
 		//return redirect('/bikegames/index')->with(compact('bike_games'));
 		return view('bike_games.index')->with(compact('bike_games'));
 	}
+
+  public function lobby(){
+    $bikeGames = BikeGame::all();
+    return view('pages.lobby', ['bikeGames'=>$bikeGames]);
+  }
 }
