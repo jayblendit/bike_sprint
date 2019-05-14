9903cd64c7b2db7e908038177ab774cef6ea6e87
CHRISTIAN <oerbachristian@gmail.com>
2019-05-14 15:13:14 +0800

    hide bikes distances menu when not logged in

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,12 +26,16 @@
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
-                <a class="navbar-brand" href="{{ route('bikes.index') }}">
-                  Bikes
-                </a>
+            @if (Auth::check())
+                <a class="navbar-brand" href="{{ url('/') }}">
+
+                <a class="navbar-brand" href="{{ url('bikeslist') }}">
+                  Bikes</a>
                 <a class="navbar-brand" href="{{ route('distances.index')}}">
-                    Distances
-                </a>
+                    Distances</a>
+            @else
+
+            @endif
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
