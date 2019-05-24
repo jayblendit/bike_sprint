6142de8a5678cafec0e79d954d11a8ff68e10bff
vccuasito <viacamillecuasito13@gmail.com>
2019-05-24 14:13:58 +0800

    update branch

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -36,7 +36,7 @@
                 </a>
                 @else
                 @endif
-            @endif
+            
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
